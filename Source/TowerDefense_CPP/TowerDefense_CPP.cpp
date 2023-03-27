0beaf9c3d53623f44ed5ba3f5567762451f94d7f
Julien PIRAT <72934794+JulienPIRAT@users.noreply.github.com>
2023-03-27 11:02:02 -0400

    [INIT] - Init Project

---
+++ Source/TowerDefense_CPP/TowerDefense_CPP.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TowerDefense_CPP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TowerDefense_CPP, "TowerDefense_CPP" );
---
+++ Source/TowerDefense_CPP/TowerDefense_CPPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TowerDefense_CPPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERDEFENSE_CPP_API ATowerDefense_CPPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
