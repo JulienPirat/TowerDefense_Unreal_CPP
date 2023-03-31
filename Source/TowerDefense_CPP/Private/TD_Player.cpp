8a24079dd2156e113e7a12408512dee2e7554466
Julien PIRAT <72934794+JulienPIRAT@users.noreply.github.com>
2023-03-31 02:36:14 -0400

    FIX Grab Mode

--- Source/TowerDefense_CPP/Private/TD_Player.cpp
+++ Source/TowerDefense_CPP/Private/TD_Player.cpp
@@ -115,26 +115,28 @@ void ATD_Player::LeftClickGrab(const FInputActionInstance& Instance)
 {
 	//isLeftClickGrab = !isLeftClickGrab;
 	//GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, TEXT("LeftClick HOLD !"));
-
-	auto playerController = UGameplayStatics::GetPlayerController(GetWorld(),0);
-	FVector CursorWorldLocation, CursorWorldDirection;
-	playerController->DeprojectMousePositionToWorld(CursorWorldLocation, CursorWorldDirection);
-	FVector CalcVector = FVector(1000.0f,1.0f,1300.0f);
-	FVector EndVector = CursorWorldLocation + (CursorWorldDirection*CalcVector);
-	FHitResult OutResult;
-	FCollisionQueryParams params;
-	params.AddIgnoredActor(this);
-	auto MainCamera = UGameplayStatics::GetActorOfClass(GetWorld(), ACameraActor::StaticClass());
-	params.AddIgnoredActor(MainCamera);
-		
-	if(GetWorld()->LineTraceSingleByChannel(OutResult, CursorWorldLocation,EndVector,ECollisionChannel::ECC_Vehicle, params, FCollisionResponseParams()))
+	if(isGrabMode)
 	{
-		if(auto DADObj = Cast<ADADObject>(OutResult.GetActor()))
+		auto playerController = UGameplayStatics::GetPlayerController(GetWorld(),0);
+		FVector CursorWorldLocation, CursorWorldDirection;
+		playerController->DeprojectMousePositionToWorld(CursorWorldLocation, CursorWorldDirection);
+		FVector CalcVector = FVector(1000.0f,1.0f,1300.0f);
+		FVector EndVector = CursorWorldLocation + (CursorWorldDirection*CalcVector);
+		FHitResult OutResult;
+		FCollisionQueryParams params;
+		params.AddIgnoredActor(this);
+		auto MainCamera = UGameplayStatics::GetActorOfClass(GetWorld(), ACameraActor::StaticClass());
+		params.AddIgnoredActor(MainCamera);
+			
+		if(GetWorld()->LineTraceSingleByChannel(OutResult, CursorWorldLocation,EndVector,ECollisionChannel::ECC_Vehicle, params, FCollisionResponseParams()))
 		{
-			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Blue, TEXT("DADOobj Reconnu !"));
-			EndVector.Z = OutResult.GetActor()->GetActorLocation().Z; // Ne peut pas spawn un block dans un mesh qui a été hit. (A SUPPR QUAND ELEVATION DES DADOBJECT EN AJOUTANT UNE COLLISION BOX)
-			DADObj->SetActorLocation(EndVector);
+			if(auto DADObj = Cast<ADADObject>(OutResult.GetActor()))
+			{
+				GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Blue, TEXT("DADOobj Reconnu !"));
+				EndVector.Z = OutResult.GetActor()->GetActorLocation().Z; // Ne peut pas spawn un block dans un mesh qui a été hit. (A SUPPR QUAND ELEVATION DES DADOBJECT EN AJOUTANT UNE COLLISION BOX)
+				DADObj->SetActorLocation(EndVector);
+			}
+			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Orange, TEXT("Hited !"));
 		}
-		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Orange, TEXT("Hited !"));
 	}
 } 
