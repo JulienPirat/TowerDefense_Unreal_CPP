e46f64e234d518d2c72602ad66ff8acb14a190f0
Julien PIRAT <72934794+JulienPIRAT@users.noreply.github.com>
2023-04-28 11:56:23 -0400

    [ADD] - BP RailGun and RailGunClass

---
+++ Source/TowerDefense_CPP/Private/DragAndDrop/RailGun_Tower.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DragAndDrop/RailGun_Tower.h"
+
+
+// Sets default values
+ARailGun_Tower::ARailGun_Tower()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Configure le MiddleBody Mesh Component
+	MiddleBody = CreateDefaultSubobject<UStaticMeshComponent>("MiddleBody");
+	MiddleBody->SetupAttachment(RootComponent);
+
+	// Configure le MiddleShield Mesh Component
+	MiddleShield = CreateDefaultSubobject<UStaticMeshComponent>("MiddleShield");
+	MiddleShield->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ARailGun_Tower::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARailGun_Tower::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/TowerDefense_CPP/Public/DragAndDrop/RailGun_Tower.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DADObject.h"
+#include "GameFramework/Actor.h"
+#include "RailGun_Tower.generated.h"
+
+UCLASS()
+class TOWERDEFENSE_CPP_API ARailGun_Tower : public ADADObject
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* MiddleBody;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* MiddleShield;
+	
+public:
+	// Sets default values for this actor's properties
+	ARailGun_Tower();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
