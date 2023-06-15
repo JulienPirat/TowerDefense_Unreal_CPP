2eec8bc5cd3d8419f127b892ffc962eebff9dd55
Julien PIRAT <72934794+JulienPIRAT@users.noreply.github.com>
2023-06-15 15:44:45 +0200

    [ADD] - Add ImpossibleCube Ennemis
    Add ImpossibleCube.cpp
    Add BP_ImpossibleCube
    Add Materials for ImpossibleCube

---
+++ Source/TowerDefense_CPP/Private/AI/ImpossibleCube.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/ImpossibleCube.h"
+
+#include "GameFramework/FloatingPawnMovement.h"
+
+
+// Sets default values
+AImpossibleCube::AImpossibleCube()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>("BoxCollision");
+	RootComponent = BoxCollision;
+
+	ImpossibleCubeMesh = CreateDefaultSubobject<UStaticMeshComponent>("ImpossibleCubeMesh");
+	ImpossibleCubeMesh->SetupAttachment(BoxCollision);
+	
+	SphereMesh = CreateDefaultSubobject<UStaticMeshComponent>("SphereMesh");
+	SphereMesh->SetupAttachment(ImpossibleCubeMesh);
+
+	// Create Movement Component for the Mignon
+	MovementComponent = CreateDefaultSubobject<UPawnMovementComponent, UFloatingPawnMovement>(TEXT("MovementComponent"));
+	MovementComponent->UpdatedComponent = RootComponent;
+}
+
+// Called when the game starts or when spawned
+void AImpossibleCube::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called to bind functionality to input
+void AImpossibleCube::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
---
+++ Source/TowerDefense_CPP/Public/AI/ImpossibleCube.h
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MignonController.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/Actor.h"
+#include "ImpossibleCube.generated.h"
+
+UCLASS()
+class TOWERDEFENSE_CPP_API AImpossibleCube : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AImpossibleCube();
+
+	UPROPERTY(BlueprintReadOnly, Category="AI")
+	AMignonController* AIController;
+
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* BoxCollision;
+	
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* ImpossibleCubeMesh;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* SphereMesh;
+	
+	UPROPERTY(Category = Pawn, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UPawnMovementComponent> MovementComponent;
+
+	/**
+	 * @brief Dégat du mignon fait au joueur
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Stat")
+	int Damage;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
